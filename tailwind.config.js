69e600ce8d9f0889709170ff24c94be8f56e8d33
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-10 16:03:38 +0500

    Added about us section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         tuscany: "#B4A495",
         "black-olive": "#3B3B3B",
         shadow: "#847366",
+        "sonic-silver": "#767676",
+        "deep-taupe": "#7D6C60",
       },
       fontFamily: {
         "logo-font": ["'Poiret One'"],
@@ -13,6 +15,9 @@ module.exports = {
       },
       boxShadow: {
         header: "2px 1px 7px 0px rgba(0, 0, 0, 0.25)",
+        about1: "1px -1px 10.1px rgba(0, 0, 0, 0.85)",
+        about2: "-20px -20px 30px 0px rgba(0, 0, 0, 0.16)",
+        btn: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       },
       backgroundImage: {
         gradient: "linear-gradient(90deg, #C0B6AA 50%, #806F62 100%)",
