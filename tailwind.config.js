80a55fa40f9cfc4883800aa1eebe2ea37b78d6c5
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-10 13:42:33 +0500

    Project init and added tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        tuscany: "#B4A495",
+        "black-olive": "#3B3B3B",
+      },
+    },
+  },
+  plugins: [],
+};
