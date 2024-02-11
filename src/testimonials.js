4436699c02f7331e6ec82009a9926bd649ee3b8c
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-11 16:35:42 +0500

    Added responsive design for testimonials.

--- src/testimonials.js
+++ src/testimonials.js
@@ -5,29 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const demoTestimonial = `
   <li
-              class="grid grid-cols-3 gap-32 bg-gradient1 rounded-[67px] scale-0 transition-transform duration-500 px-8 py-9"
+              class="laptopSm:grid laptopSm:grid-cols-3 gap-8 mobile:gap-12 tablet:gap-16 laptopSm:gap-24 laptopMd:gap-32 bg-gradient1 rounded-[67px] scale-0 transition-transform duration-500 px-8 py-9"
             >
-              <div class="relative flex gap-12">
-                <div class="w-1/4 bg-white h-3/5 rounded-[100px]"></div>
-                <div class="w-1/4 h-full bg-white rounded-[100px]"></div>
+              <div class="relative flex flex-col gap-4 laptopSm:flex-row laptopSm:gap-12 max-laptopSm:h-48">
+                <div class="w-3/5 bg-white h-1/4 rounded-[100px] laptopSm:w-1/4 laptopSm:h-3/5"></div>
+                <div class="w-full h-1/4 laptopSm:h-3/4 laptopMd:h-full laptopSm:w-1/4 bg-white rounded-[100px]"></div>
                 <img
                   src="../images/testimonial.png"
                   alt="customer"
-                  class="absolute top-1/3 left-[38%]"
+                  class="absolute -top-5 right-1/4 laptopSm:top-1/3 laptopSm:left-[38%]"
                 />
               </div>
               <div
-                class="flex flex-col col-span-2 gap-5 relative before:content-[url('../images/open-quote.png')] after:content-[url('../images/close-quote.png')] after:absolute after:-bottom-11 after:right-0 mt-20 mb-16"
+                class="flex flex-col col-span-2 gap-5 relative before:content-[url('../images/open-quote.png')] after:content-[url('../images/close-quote.png')] after:absolute after:-bottom-11 after:right-0 mt-12 laptopSm:mt-20 laptopSm:mb-16 mb-12"
               >
                 <div>
                   <p class="text-[#414141] text-xl">Leslie Alexander</p>
                   <p class="text-white">Moncton, Canada</p>
                 </div>
                 <div class="font-['Montserrat']">
-                  <p class="text-4xl font-semibold text-black">
+                  <p class="text-xl mobile:text-2xl tablet:text-3xl laptopSm:text-4xl font-semibold text-black">
                     Neque porro quisquam est qui dolum
                   </p>
-                  <p class="text-xl text-white">
+                  <p class="mobile:text-lg tablet:text-xl text-white">
                     “ It is a long established fact that a reader will be
                     tracked distracted by the readable content of a page is when
                     looking at its layout. The point of using Lorem of
